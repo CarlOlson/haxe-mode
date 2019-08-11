bc202cc3ea75d19eb605291a79534c672bea5125
CarlOlson <CarlOlson@users.noreply.github.com>
2019-08-12 01:33:52 +0900

    fix code style and docs

--- haxe-mode.el
+++ haxe-mode.el
@@ -1,11 +1,29 @@
-;;; haxe-mode.el --- Major mode for Haxe
+;;; haxe-mode.el --- Major mode for Haxe -*- lexical-binding: t -*-
 
-;;; Version: 0.0.1
+;; Author: Carl Olson <>
+;; URL: https://github.com/CarlOlson/haxe-mode
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.3"))
 
 ;;; Commentary:
-;;
 
-(defconst haxe-mode-syntax-table
+;; There are other major modes for haxe, this package attempts to
+;; provide a simple, modern, and (somewhat) tested alternative.
+;; Currently supports Haxe 4.0 keywords.
+
+;; This package is complete and usable.  For support and feature
+;; requests please create an issue on github.  PRs are welcome, but
+;; consider creating a minor mode for features like autocomplete and
+;; linting instead.
+
+;;; Code:
+
+(defcustom haxe-mode-indent-level 2
+  "Number of spaces per indent level for ‘haxe-mode’."
+  :type 'integer
+  :group 'haxe)
+
+(defvar haxe-mode-syntax-table
   (let ((table (make-syntax-table)))
     (modify-syntax-entry ?' "\"" table)     ;; single quote string
     (modify-syntax-entry ?\" "\"" table)    ;; double quote string
@@ -13,9 +31,9 @@
     (modify-syntax-entry ?* ". 23" table)   ;; /* comment
     (modify-syntax-entry ?\n "> b" table)   ;; // comment eol
     table)
-  "Syntax table for haxe-mode.")
+  "Syntax table for ‘haxe-mode’.")
 
-(defconst haxe-mode-font-lock-keywords
+(defvar haxe-mode-font-lock-keywords
   (let* ((keywords '(abstract cast class dynamic enum extends
                      extern false final final function implements
                      import in inline interface macro new null
@@ -23,7 +41,7 @@
                      static this true typedef untyped var))
          (controls '(break case catch continue default do else
                      for if return switch throw try while))
-         (keyword-strings (mapcar 'symbol-name (append keywords controls)))
+         (keyword-strings (mapcar #'symbol-name (append keywords controls)))
          (keywords-regexp (regexp-opt keyword-strings 'words))
          (annotation-regexp   (rx "@:" (group (+ word))))
          (regexp-regexp       (rx "~/" (* (not (any "/"))) "/"))
@@ -37,19 +55,7 @@
       (,preprocessor-regexp . font-lock-preprocessor-face)
       (,keywords-regexp . font-lock-keyword-face)
       (,type-regexp . font-lock-type-face)))
-  "Font lock configuration for haxe-mode.")
-
-(defconst haxe-mode-indent-level 2
-  "Number of spaces per indent level for haxe-mode.")
-
-(define-derived-mode haxe-mode prog-mode "Haxe"
-  "A major mode for editing Haxe files."
-  :syntax-table haxe-mode-syntax-table
-  (setq-local comment-start "// ")
-  (setq-local comment-start-skip (rx "//" (* (or "//" whitespace))))
-  (setq-local indent-line-function 'haxe-indent-line)
-  (setq-local font-lock-defaults '(haxe-mode-font-lock-keywords))
-  (font-lock-fontify-buffer))
+  "Font lock configuration for ‘haxe-mode’.")
 
 (defun haxe-indent-line ()
   "Indent current line of Sample code."
@@ -66,6 +72,18 @@
 (defun haxe-comment-or-string ()
   "Is point in a comment or string?"
   (member (syntax-ppss-context (syntax-ppss)) '(comment string)))
+
+;;;###autoload
+(define-derived-mode haxe-mode prog-mode "Haxe"
+  "A major mode for editing Haxe files."
+  :syntax-table haxe-mode-syntax-table
+  (setq-local comment-start "// ")
+  (setq-local comment-start-skip (rx "//" (* (or "//" whitespace))))
+  (setq-local indent-line-function #'haxe-indent-line)
+  (setq-local font-lock-defaults '(haxe-mode-font-lock-keywords)))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist `(,(rx ".hx" eol) . haxe-mode))
 
 (provide 'haxe-mode)
 
