fc86d49af6c05f9196231c2d0dd6e3d6dc5a23a8
CarlOlson <CarlOlson@users.noreply.github.com>
2019-08-11 20:00:50 +0900

    add sample file for manual checking behaviour

---
+++ samples/Main.hx
@@ -0,0 +1,28 @@
+// a comment
+
+package;
+
+/* another
+ * comment
+ */
+
+class Main {
+
+  var name: String;
+
+  function main():Void {
+    name = "carl" + 'olson';
+
+    var regexp:EReg = ~/world/;
+
+    #if debug
+    trace("hello");
+    #end
+
+    var map = [1 => 101, 2 => 102, 3 => 103];
+    for (key => value in map) {
+      trace(key, value);
+    }
+  }
+
+}
